dab81db75517f769467db01f34c2dfcd3c60fabd
PEP 8 Speaks <pep8speaks@gmail.com>
2019-10-15 01:00:07 +0530

    Fix PEP 8 errors in dakota/oat/fleet-share/fleet-share.py

--- dakota/oat/fleet-share/fleet-share.py
+++ dakota/oat/fleet-share/fleet-share.py
@@ -1,11 +1,11 @@
+import glob
+import subprocess
+import dakota.interfacing as di
+import input as inp
 import sys
 import os
 sys.path.append('../../../scripts')
-import input as inp
 # import output as oup
-import dakota.interfacing as di
-import subprocess
-import glob
 
 cycdir = '../../../cyclus-files/oat/fleet-share/'
 
@@ -20,14 +20,14 @@ params, results = di.read_parameters_file()
 # -------------------------------
 
 # Edit Cyclus input file
-cyclus_template = cycdir+'fleet-share.xml.in'
-scenario_name = 'fs' + str(int(params['fs'])) #+ '-exp'
-variable_dict = {'fleet_share_mox': int((params['fs'])),'fleet_share_fr':int((100-params['fs']))}
-output_xml = cycdir+'fleet-share.xml'
+cyclus_template = cycdir + 'fleet-share.xml.in'
+scenario_name = 'fs' + str(int(params['fs']))  # + '-exp'
+variable_dict = {'fleet_share_mox': int((params['fs'])), 'fleet_share_fr': int((100 - params['fs']))}
+output_xml = cycdir + 'fleet-share.xml'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Run Cyclus with edited input file
-output_sqlite = cycdir+scenario_name+'.sqlite'
+output_sqlite = cycdir + scenario_name + '.sqlite'
 os.system('cyclus -i ' + output_xml + ' -o ' + output_sqlite)
 # ----------------------------
 # Return the results to Dakota
