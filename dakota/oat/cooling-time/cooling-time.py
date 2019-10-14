af819d7c39e8209f9250319ba0a23196398fc88f
gwen <gwendolynchee95@gmail.com>
2019-10-14 14:25:28 -0500

    remove comments

--- dakota/oat/cooling-time/cooling-time.py
+++ dakota/oat/cooling-time/cooling-time.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.append('../../../scripts')
 import input as inp
-# import output as oup
 import dakota.interfacing as di
 import subprocess
 import glob
@@ -36,6 +35,5 @@ os.system('cyclus -i ' + output_xml + ' -o ' + output_sqlite)
 for i, r in enumerate(results.responses()):
     if r.asv.function:
         r.function = 1
-        print('OUT', i, r.function)
 
 results.write()
