ea0ad07db53580e84efdaa0494d7e2705f6f79e1
PEP 8 Speaks <pep8speaks@gmail.com>
2019-10-15 01:00:08 +0530

    Fix PEP 8 errors in dakota/oat/transition-year/transition-year.py

--- dakota/oat/transition-year/transition-year.py
+++ dakota/oat/transition-year/transition-year.py
@@ -1,10 +1,10 @@
+import glob
+import subprocess
+import dakota.interfacing as di
+import input as inp
 import sys
 import os
 sys.path.append('../../../scripts')
-import input as inp
-import dakota.interfacing as di
-import subprocess
-import glob
 
 cycdir = '../../../cyclus-files/oat/transition-year/'
 
@@ -19,14 +19,14 @@ params, results = di.read_parameters_file()
 # -------------------------------
 
 # Edit Cyclus input file
-cyclus_template = cycdir+'transition-year.xml.in'
-scenario_name = 'ty' + str(int(params['ty'])) #+ '-exp'
+cyclus_template = cycdir + 'transition-year.xml.in'
+scenario_name = 'ty' + str(int(params['ty']))  # + '-exp'
 variable_dict = {'transition_year': int((params['ty']))}
-output_xml = cycdir+'transition-year.xml'
+output_xml = cycdir + 'transition-year.xml'
 inp.render_input(cyclus_template, variable_dict, output_xml)
 
 # Run Cyclus with edited input file
-output_sqlite = cycdir+scenario_name+'.sqlite'
+output_sqlite = cycdir + scenario_name + '.sqlite'
 os.system('cyclus -i ' + output_xml + ' -o ' + output_sqlite)
 # ----------------------------
 # Return the results to Dakota
