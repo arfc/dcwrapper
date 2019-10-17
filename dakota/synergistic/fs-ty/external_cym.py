750667a86b0275e0ec32ab7cc0bb5565cc3e6c21
gwen <gwendolynchee95@gmail.com>
2019-10-17 14:45:28 -0500

    synergistic

---
+++ dakota/synergistic/fs-ty/external_cym.py
@@ -0,0 +1,55 @@
+import cymetric as cym
+from cymetric import timeseries
+import signal
+import os
+import sys
+sys.path.append('../../../scripts')
+import output as oup
+
+
+def hlw():
+    f = open('output_name.txt','r')
+    if f.mode == 'r': 
+        output_sqlite = f.read()
+    f.close()
+    print('inhlw')
+    #output_sqlite = 'wow.sqlite'
+    ev = cym.Evaluator(db=cym.dbopen(output_sqlite),write=True)
+    val = cym.timeseries.transactions(ev,commodities=['lwrreprocessingwaste','moxreprocessingwaste','frreprocessingwaste'])['Mass'].cumsum().iloc[-1]
+    print(val)
+    with open('hlw.txt','w') as f: 
+        f.write(str(val))
+    return val
+
+
+def dep_u():
+    f = open('output_name.txt','r')
+    if f.mode == 'r': 
+        output_sqlite = f.read()
+    f.close()
+    
+    print('indep')
+    #output_sqlite = 'wow.sqlite'
+    ev = cym.Evaluator(db=cym.dbopen(output_sqlite),write=True)
+    val = cym.timeseries.transactions(ev,commodities=['enrichmentwaste'])['Mass'].cumsum().iloc[-1]
+    print(val)
+    f = open('depu.txt','w+')
+    f.write(str(val))
+    f.close()
+    return val
+
+
+def idlecapp():
+    f = open('output_name.txt','r')
+    if f.mode == 'r': 
+        output_sqlite = f.read()
+    f.close()
+    print('inidle')
+    ev = cym.Evaluator(db=cym.dbopen(output_sqlite),write=True)
+    print(ev)
+    val, val2, val3 =oup.idlecap(ev,'(60000+250*t/12)/1000')
+    f = open('idlecap.txt','w+')
+    f.write(str(val))
+    f.close()
+    print(val)
+    return val
